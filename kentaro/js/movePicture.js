383c5cbd372517ad77d46ad8664d3d3994883794
Drew Volz <volz.drew@gmail.com>
2022-02-04 19:24:11 -0800

    launch v2.0 of catch kentaro
    * saved score loading
    * mobile friendly for tap events
    * background color size fix
    * callout to other kentaro games

--- kentaro/js/movePicture.js
+++ kentaro/js/movePicture.js
@@ -1,33 +1,69 @@
-$( document ).ready(function() {
-	jQuery(function($) {
-	    $('#kentaro').mouseover(function() {
+$(document).ready(function () {
+	let safeColors = ['00', '33', '66', '99', 'cc', 'ff']
 
-	    	var safeColors = ['00','33','66','99','cc','ff'];
-			var rand = function() {
-			    return Math.floor(Math.random()*6);
-			};
-			var randomColor = function() {
-			    var r = safeColors[rand()];
-			    var g = safeColors[rand()];
-			    var b = safeColors[rand()];
-			    return "#"+r+g+b;
-			};
+	let rand = () => {
+		return Math.floor(Math.random() * 6)
+	}
 
-			var theBody = $( "body" );
-	    	var theScoreEl = $( "#score" );
-	    	var tempScoreNum = parseInt(theScoreEl.text());
-	    	tempScoreNum++;
-	    	theScoreEl.text(tempScoreNum);
-	    	$(theBody).css('background',randomColor());
-	    	$(theBody).css({color:'white'},1000);
+	let randomColor = () => {
+		let r = safeColors[rand()]
+		let g = safeColors[rand()]
+		let b = safeColors[rand()]
+		return '#' + r + b + g
+	}
 
+	let getScoreFromDom = () => {
+		return document.getElementById('score')
+	}
 
-	        var dWidth = $(document).width() - 211, // image width
-	            dHeight = $(document).height() - 211, // image height
-	            nextX = Math.floor(Math.random() * dWidth),
-	            nextY = Math.floor(Math.random() * dHeight);
-	        $(this).animate({ left: nextX + 'px', top: nextY + 'px' });
-	    });
-	});
-});
+	let saveFile = () => {
+		let score = getScoreFromDom().textContent
+		if (parseInt(score) > 0) {
+			localStorage.setItem('kentaro.saveFile', score)
+		}
+	}
 
+	let loadFile = () => {
+		var score = JSON.parse(localStorage.getItem('kentaro.saveFile'))
+		if (score !== null) {
+			getScoreFromDom().textContent = score
+		}
+	}
+
+	function updateState() {
+		let scoreElement = getScoreFromDom()
+		let score = parseInt(scoreElement.textContent)
+
+		score++
+		scoreElement.textContent = score
+
+		saveFile()
+
+		document.body.style.background = randomColor()
+		document.body.style.color = 'white'
+
+		let dWidth = $(document).width() - 211
+		let dHeight = $(document).height() - 211
+
+		let nextX = Math.floor(Math.random() * dWidth)
+		let nextY = Math.floor(Math.random() * dHeight)
+
+		$(this).animate({ left: nextX + 'px', top: nextY + 'px' })
+	}
+
+	jQuery(function ($) {
+		loadFile()
+
+		const isTouchDevice = () => {
+			return ['iPad', 'iPhone', 'iPod'].includes(navigator.platform) || (navigator.userAgent.includes("Mac") && "ontouchend" in document)
+		}
+
+		// bluntly assume only one input method for mobile as mouseup
+		// and mouseover can lead to 2 taps
+		if(isTouchDevice()) {
+			$('#kentaro').on('mouseup', updateState)
+		} else {
+			$('#kentaro').on('mouseover', updateState)
+		}
+	})
+})
